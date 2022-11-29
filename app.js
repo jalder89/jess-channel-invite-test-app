221bd9847f61dd60cb6805794f513bf593f5960b
Jessica Barnes <wbarnes@slack-corp.com>
2022-11-29 11:58:09 -0800

    Implemented select member inputs in modal for owner and default invite member.  Implemented default member invitation after channel creation. Cleaned up new invitee greeting.

--- app.js
+++ app.js
@@ -43,16 +43,28 @@ app.command('/create', async ({ command, ack, client }) => {
                     },
                     {
                         "type": "input",
-                        "block_id": "creator_input",
+                        "block_id": "owner_user_select",
                         "element": {
-                            "type": "plain_text_input",
-                            "action_id": "creator_name"
+                            "type": "users_select",
+                            "action_id": "owner_name"
                         },
                         "label": {
                             "type": "plain_text",
-                            "text": "Creator"
+                            "text": "Owner"
                         }
                     },
+                    {
+                        "type": "input",
+                        "block_id": "default_user_select",
+                        "element": {
+                            "type": "users_select",
+                            "action_id": "invite_default_user_select"
+                        },
+                        "label": {
+                            "type": "plain_text",
+                            "text": "Select a default user to invite"
+                        }
+                    }
                 ],
                 "submit": {
                     "type": "plain_text",
@@ -80,11 +92,18 @@ app.view('create_channel_view_1', async ({ ack, view, client }) => {
         const result2 = await client.chat.postMessage({
             token: process.env.SLACK_BOT_TOKEN,
             channel: result.channel.id,
-            text: `This channel was created by ${view.state.values.creator_input.creator_name.value}`
+            text: `This channel is owned by <@${view.state.values.owner_user_select.owner_name.selected_user}>`
+        });
+
+        // Invite the user selected in modal to the channel
+        // Call the conversations.invite method using the built-in WebClient
+        const result3 = await client.conversations.invite({
+            channel: result.channel.id,
+            users: view.state.values.default_user_select.invite_default_user_select.selected_user
         });
     } catch (error) {
         console.error(error);
-    }
+    } 
 });
 
 // event listener that listens for member_joined_channel events
@@ -93,7 +112,7 @@ app.event('member_joined_channel', async ({ event, client }) => {
         // Call the chat.postMessage method using the built-in WebClient
         const result = await client.chat.postMessage({
             channel: event.channel,
-            text: `Welcome to the channel, <@${event.user}>! We're here to help. :tada:`
+            text: `Welcome to the channel, <@${event.user}>! :tada:`
         });
     }
     catch (error) {
